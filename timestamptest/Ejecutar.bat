6ba108d051956f1b1255efa7815d2f3b33888481
acost <acost@LAPTOP-JCLKGHPA>
2017-05-15 22:43:53 -0500

    Dockerfile

--- timestamptest/Ejecutar.bat
+++ timestamptest/Ejecutar.bat
@@ -12,7 +12,7 @@ echo Executing timestamp-test image:
 echo ---------------------------------------
 echo ---------------------------------------
 pause
-docker login
-docker tag timestamp-test mfrondon/timestamp-test:1.0
-docker push mfrondon/timestamp-test:1.0
+REM docker login
+REM docker tag timestamp-test mfrondon/timestamp-test:1.0
+REM docker push mfrondon/timestamp-test:1.0
 docker run -p 4000:8080 mfrondon/timestamp-test:1.0
