0f4615a0ae642efa6d9def254e89f20a4d313e3e
Michael Felipe Rondón Acosta <mfrondona@unal.edu.co>
2017-05-15 23:32:59 -0500

    Update Ejecutar.bat

--- timestamptest/Ejecutar.bat
+++ timestamptest/Ejecutar.bat
@@ -6,13 +6,3 @@ echo ---------------------------------------
 echo ---------------------------------------
 pause
 docker build -t timestamp-test .
-echo ---------------------------------------
-echo ---------------------------------------
-echo Executing timestamp-test image:
-echo ---------------------------------------
-echo ---------------------------------------
-pause
-REM docker login
-REM docker tag timestamp-test mfrondon/timestamp-test:1.0
-REM docker push mfrondon/timestamp-test:1.0
-docker run -p 4000:8080 mfrondon/timestamp-test:1.0
